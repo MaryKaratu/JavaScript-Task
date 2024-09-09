9b6cee1e57971c366c13507b5bd9da159ab07ab6
KaratuMary <karatumary85@gmail.com>
2024-09-09 22:58:37 +0100

    variables

--- assignment.js
+++ assignment.js
@@ -10,3 +10,13 @@ let language;
 console.log(typeof isLand);
 console.log(typeof population);
 console.log(typeof country);
+
+Const language = "English";
+const country = "Nigeria";
+const continent = "African continent";
+const isLand = false;
+// isLand = true; const cannot be reassigned another value
+console.log(typeof language);
+console.log(country);
+console.log(continent);
+console.log(typeof isLand);
