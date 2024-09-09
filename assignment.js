611c874dc45e684600b9f4c83ca66d3da5698ced
KaratuMary <karatumary85@gmail.com>
2024-09-09 22:51:34 +0100

    data-types

--- assignment.js
+++ assignment.js
@@ -4,3 +4,9 @@ let population = 230000000;
 console.log(country);
 console.log(continent);
 console.log(population);
+
+let isLand = false;
+let language;
+console.log(typeof isLand);
+console.log(typeof population);
+console.log(typeof country);
