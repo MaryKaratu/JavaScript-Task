d13b82634440be1f48dba932d0354451ccc8a7ad
KaratuMary <karatumary85@gmail.com>
2024-09-09 23:03:05 +0100

    operators

--- assignment.js
+++ assignment.js
@@ -20,3 +20,24 @@ console.log(typeof language);
 console.log(country);
 console.log(continent);
 console.log(typeof isLand);
+
+population = 230000000 / 2;
+population++;
+console.log(population);
+console.log(population > 6000000);
+console.log(population < 33000000);
+const description =
+  country +
+  " " +
+  "is in" +
+  " " +
+  continent +
+  " " +
+  "and its" +
+  " " +
+  population +
+  " " +
+  "people speak" +
+  " " +
+  language;
+console.log(description);
